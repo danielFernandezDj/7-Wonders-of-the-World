f37bdc0c9225318615048d2d4f4d7c57880f68fd
danielFernandezJd <mdanif9728@gmail.com>
2024-04-13 15:47:17 -0700

    first saving

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('Hello!')
+// Select the card storage section. 
+const cardStorage = document.getElementById('card')
+
+// Select the Images in the storage.
+const imgEgypt = document.getElementById("egypt")
+cardStorage.appendChild(imgEgypt)
+
+// Select Q&A Text.
+const textH3 = document.getElementById('h3')
+
+// Select Inputs.
+const inputOne = document.getElementById('option-one')
+const inputTwo = document.getElementById('option-two')
+const inputThree = document.getElementById('option-three')
+const inputFour = document.getElementById('option-four')
+
+// Variables.
+let winButton = false;
+
+//  Input! ••••••••••••••••••••••••••••••••••••
+function Egypt() {
+  imgEgypt.style.display = "block"
+
+  textH3.textContent = 'From what country is "The Great Pyramid of Giza"'
+
+  inputOne.textContent = 'Egypt';
+  inputTwo.textContent = 'Vegas';
+  inputThree.textContent = 'India';
+  inputFour.textContent = 'Pakistan';
+
+  [inputTwo, inputThree, inputFour].forEach(function (input) {
+    input.addEventListener('click', function (event) {
+      console.log("Button" + event.target.id  + "is pressed.");
+    });
+  });
+  
+  inputOne.addEventListener('click', () => {
+    winButton = true; 
+    console.log(`The win button was press it =  ${winButton}`)
+  })
+}
+Egypt();
+
+
+
+
