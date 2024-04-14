569af7d78440a691ba661a4c801910dbfa617345
danielFernandezJd <mdanif9728@gmail.com>
2024-04-13 19:39:53 -0700

    first saving

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const inputFour = document.getElementById('option-four')
 
 // Variables.
 let winButton = false;
+let timeOut = false;
 
 //  Input! ••••••••••••••••••••••••••••••••••••
 function Egypt() {
@@ -42,24 +43,38 @@ function Egypt() {
 Egypt();
 
 
-// let timeVar = document.getElementById("time-var");
-// let timeProgress = document.getElementById('time-var-progress')
+// Time var section.
+let timeVar = document.getElementById("time-var");
+let timeProgress = document.getElementById('time-var-progress')
 
-// function countdown() {
-//     let initialWidth = parseFloat(window.getComputedStyle(timeVar).width); 
-//     let i = initialWidth;
-//     let decrementAmount = initialWidth / 10; // You can adjust this value as needed
-
-//     let interval = setInterval(function() {
-//         timeProgress.style.width = i + "px"; // Set the width of timeProgress
-//         console.log(i);
-//         i -= decrementAmount;
-//         if (i < 0) {
-//             clearInterval(interval);
-//             console.log("Blast off!");
-//         }
-//     }, 50); // Adjust the delay here (in milliseconds)
-// }
+function countdown() {
+    let i = parseFloat(window.getComputedStyle(timeVar).width) 
+    let interval = setInterval(function() {
+        timeProgress.style.width = i + "px"; // Set the width of timeVar
+        console.log(i);
+        i--;
+        if (i < 0) {
+            clearInterval(interval);
+            timeOut = true
+            console.log("Blast off!");
+        }
+        if (i === 150) {
+            timeProgress.style.backgroundColor = 'red'
+        } 
+        // Reduce the time speed wen the player get Points.
+    }, 50); // Adjust the delay here (in milliseconds)
+}
 
 // countdown();
 
+
+// TODO: add a video button for the card; example: a youtube video from Egypt.
+// Lose conditions:
+// If you select the wrong option you lose 1live; => and change to a random card.
+// if the time is 0 you lose 1live; => and change to a random card.
+
+// Win condition:
+// You get one star if player select the right option;
+// => then delete this card from the array - and change to a random card. 
+// Player wins wen get the total of 7 stars.
+// => then Pop Up your Win
